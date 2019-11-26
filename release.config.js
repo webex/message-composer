28cd8167127fa893fcb1a10cf40468f67264b64f
aimma <aimma@cisco.com>
2019-11-26 15:50:47 -0800

    chore(yarn): add commitlint and semantic release

---
+++ release.config.js
@@ -0,0 +1,21 @@
+/*
+ * we use semantic-release to automate the whole
+ * package release workflow and the options,
+ * mode and plugins can be set here
+ * more info: https://github.com/semantic-release/semantic-release
+ */
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog'
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'yarn.lock'],
+      },
+    ],
+    '@semantic-release/github',
+  ]
+};
