c9a15e7e0769a4996d113b0c26db9aa7e2157f8f
aimma <aimma@cisco.com>
2019-11-26 12:09:38 -0800

    chore(yarn): use yarn and add peer dependencies

--- dist/plugins/mentions/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.CAPTURE_REGEX=exports.USER_MENTION_NODE_TYPE=exports.CONTEXT_MARK_TYPE=void 0;/**
- * The decoration mark type that the menu will position itself against.
- * @type {String}
- */var CONTEXT_MARK_TYPE="mentionContext";exports.CONTEXT_MARK_TYPE="mentionContext";var USER_MENTION_NODE_TYPE="userMention";/**
- * The regex to use to find the searchQuery.
- *
- * @type {RegExp}
- */exports.USER_MENTION_NODE_TYPE="userMention";var CAPTURE_REGEX=/(@)([^@\s]*[ ]?[^@\s]*)$/;exports.CAPTURE_REGEX=CAPTURE_REGEX;
