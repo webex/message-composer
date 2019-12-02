cdcd632b49478829d32a45b720a2393ad49b859d
aimma <aimma@cisco.com>
2019-12-02 13:33:44 -0800

    fix(semantic-release): fix syntax error

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog'
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     [
       '@semantic-release/git',
